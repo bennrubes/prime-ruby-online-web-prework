b92b686482359b4983d8278404a2627c73b25433
Sam Bennrubi <sbennrubi@gmail.com>
2018-10-30 22:02:41 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2 || num == 3
+   return true
+  elsif
+    x = 2
+    while x <= (num/2.0)
+        num % x == 0
+      return false
+    end
+  else
+    return true
+ end
+end
